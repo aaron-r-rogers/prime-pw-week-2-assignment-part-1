f8e2012ebd5a6e5e2e870e77cfdef5f5019ae17b
aaron-r-rogers <rogers.aaron.r@gmail.com>
2021-11-03 14:32:22 -0500

    16 and 17 done, 18 is last stretch goal

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -58,7 +58,24 @@ if (pets < allowedPets) {
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios.
 // console.log `mostPets` after the conditional has run.
-
+let mostPets
+if (pets >= friendsPets) {
+  mostPets = pets;
+} else if (pets <= friendsPets) {
+  mostPets = friendsPets;
+}
+console.log(mostPets);
 // 17 - Rewrite question 16 with a `switch` statement. You'll need to do research!
+switch (true) {
+  case (friendsPets >= pets):
+  mostPets = friendsPets;
+    break;
+  case (pets >= friendsPets):
+  mostPets = pets;
+    break;
+  default:
+  console.log('error');
+}
 
+console.log(mostPets)
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
